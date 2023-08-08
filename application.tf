9a52c7b9abc9e4f3848016225da46044e5909207
Bhargavi <bhargavimandala@gmail.com>
2023-08-08 13:08:38 +0100

    aws beanstalk setup

---
+++ application.tf
@@ -0,0 +1,78 @@
+resource "aws_elastic_beanstalk_application" "test" {
+  name        = "test-application"
+}
+
+# Create elastic beanstalk Environment
+ 
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+  name                = "myenvironment"
+  application         = aws_elastic_beanstalk_application.test.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.3 running Python 3.11"
+  tier                = var.tier 
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "vpc-0e2534926c09306d9"
+  }
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     =  "aws-elasticbeanstalk-ec2"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "AssociatePublicIpAddress"
+    value     =  "True"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", var.public_subnets)
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment:process:default"
+    name      = "MatcherHTTPCode"
+    value     = "200"
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t3.micro"
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "ELBScheme"
+    value     = "internet facing"
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = 1
+  }
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MaxSize"
+    value     = 2
+  }
+  setting {
+    namespace = "aws:elasticbeanstalk:healthreporting:system"
+    name      = "SystemType"
+    value     = "enhanced"
+  }
+ 
+}
+
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "tier" {
+  default = "WebServer"
+}
+
+variable "public_subnets" {
+    default = ["subnet-0833c215b4b1a321c", "subnet-01870acb388940d14"]
+}
+
+
+
